6503d3b6a499c734d1b8c64222a75079f95ab4cd
Vassil Mladenov <vmladenov@icloud.com>
2016-02-16 23:36:49 -0600

    unsafe commit, probably doesn't compile, don't revert to this commit if testing

--- mp1.c
+++ mp1.c
@@ -2,6 +2,7 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/string.h> 
 #include "mp1_given.h"
 
 MODULE_LICENSE("GPL");
@@ -10,6 +11,67 @@ MODULE_DESCRIPTION("CS-423 MP1");
 
 #define DEBUG 1
 
+#ifndef LIST_NODE_H
+#define LIST_NODE_H 
+
+typedef struct node
+{
+   struct list_head list;
+   int pid;
+   int cputime;
+} list_node;
+#endif
+
+static LIST_HEAD(head);
+
+#define FILENAME "status"
+#define DIRECTORY "mp1"
+
+static struct proc_dir_entry *proc_dir;
+static struct proc_dir_entry *proc_entry;
+static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data){
+   // implementation goes here...
+   int copied;
+   char * buf;
+   buf = (char *) kmalloc(count,GFP_KERNEL); 
+   copied = 0;
+   //... put something into the buf, updated copied 
+   copy_to_user(buffer, buf, copied);
+   kfree(buf);
+   return copied ;
+}
+static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data){ 
+   // implementation goes here...
+   int copied;
+   char * buf;
+   buf = (char *) kmalloc(count,GFP_KERNEL); 
+   copied = 0;
+   //... put something into the buf, updated copied 
+   copy_from_user(buf, buffer, count);
+
+   // get pid from char *
+   int pid;
+   kstrtoint(buffer, 10, &pid);
+   // find the node that corresponds to this list node
+
+   list_node * new_node = kmalloc(sizeof(list_node), GFP_KERNEL);
+   memset(new_node, 0, sizeof list_node);
+
+   list_add(&new_node->list, &head);
+
+   // if it doesn't exist, create it
+
+
+   kfree(buf);
+   return count;
+}
+static const struct file_operations mp1_file = {
+   .owner = THIS_MODULE, 
+   .read = mp1_read,
+   .write = mp1_write,
+};
+int init mp1_init(void){
+}
 // mp1_init - Called when module is loaded
 int __init mp1_init(void)
 {
@@ -18,8 +80,9 @@ int __init mp1_init(void)
    #endif
    // Insert your code here ...
    
-   
-   
+      
+   proc_dir = proc_mkdir(DIRECTORY, NULL);
+   proc_entry = proc_create(FILENAME, 0666, proc_dir, & mp1_file); 
    printk(KERN_ALERT "MP1 MODULE LOADED\n");
    return 0;   
 }
--- userapp.c
+++ userapp.c
@@ -1,6 +1,19 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
 #include "userapp.h"
+
 
 int main(int argc, char* argv[])
 {
+	// get pid 
+	int pid = getpid();
+	// write pid as a string to the proc filesystem
+	FILE * statusfile = fopen("/proc/mp1/status", "r+");
+	fprintf(statusfile, "%d", pid);
+	// read the proc fs
+
+	fclose(statusfile);
 	return 0;
 }
